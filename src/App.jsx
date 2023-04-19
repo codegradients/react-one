ca1a9222679d366c6bcee9f1a49b64979c7bcfbb
{shah0221} <{my>
2023-04-19 15:41:42 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./styles/App.scss";
+import "./styles/header.scss";
+import "./styles/home.scss";
+import "./styles/contact.scss";
+import "./styles/footer.scss";
+import "./styles/mediaquery.scss";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
